854f08c2232a8d28d4c44e7f24aab911fb32cc74
Mojiz Mohammed Abdul <mojizma@gmail.com>
2015-12-29 16:12:46 -0500

    my first commit

---
+++ 000-app.js
@@ -0,0 +1,5 @@
+var app = angular.module("java4sApp", []);
+ 
+app.controller("HelloWorldController", function($scope) {
+                $scope.message = "Welcome to Java4s AngularJS Tutorials";                       
+});
